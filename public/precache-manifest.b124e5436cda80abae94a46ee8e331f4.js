782f441d945777d358071b4147560756429cdf97
Zeswen <zeswentyrael@gmail.com>
2018-12-19 13:25:02 +0100

    Fixed changes

---
+++ public/precache-manifest.b124e5436cda80abae94a46ee8e331f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c8c828f90ac6f178e19",
+    "url": "/static/css/main.363b5bf4.chunk.css"
+  },
+  {
+    "revision": "7c8c828f90ac6f178e19",
+    "url": "/static/js/main.7c8c828f.chunk.js"
+  },
+  {
+    "revision": "daf14e67f6226b267fae",
+    "url": "/static/css/1.8c899bd3.chunk.css"
+  },
+  {
+    "revision": "daf14e67f6226b267fae",
+    "url": "/static/js/1.daf14e67.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7554347a31b0c9894e53b2dd2fad500b",
+    "url": "/static/media/landscape2.7554347a.jpg"
+  },
+  {
+    "revision": "8ce9b3a2d80694bf7adad39b67544803",
+    "url": "/static/media/logo.8ce9b3a2.svg"
+  },
+  {
+    "revision": "a387cd153bd883ec7daaaffe34963785",
+    "url": "/index.html"
+  }
+];
