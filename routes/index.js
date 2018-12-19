6d3a341c0b3c8aa1b62fd8e98fac8eeeb15acc8e
Zeswen <zeswentyrael@gmail.com>
2018-12-19 14:04:39 +0100

    Fixed Deploy

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router  = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
-
 router.use('/api/auth', require('./auth'));
 
 router.use('/api/pages', require('./pages'));
